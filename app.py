8b5b6a648bb226c232da2d6a089079e590cb587c
cli48 <121372903+cli48@users.noreply.github.com>
2025-11-26 04:42:11 -0500

    format json
    json

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify({"message": "Hello World"}), 200
